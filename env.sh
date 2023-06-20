9eeb8d18329ed8dca206fc5f1f728b8940e8ba2f
Suresh Khatri <servlet2@msn.com>
2023-06-20 11:56:23 +1000

    update env

--- env.sh
+++ env.sh
@@ -1,12 +1,14 @@
 cp hive/conf/hive-site-template.xml hive/conf/hive-site.xml
 while read line; 
 do 
-    read a b <<< $(echo $line | awk -F"=" '{print $1" "$2}')
-    if [[ $a != "" ]];
-    then
+    if [[ ! $line =~ ^# ]]; then
+      read a b <<< $(echo $line | awk -F"=" '{print $1" "$2}')
+      if [[ $a != "" ]];
+      then
         echo replacing \${env:$a} with $b
         sed s#"\${env:$a}"#"$b"#g hive/conf/hive-site.xml > hive/conf/.tmp
         cp hive/conf/.tmp hive/conf/hive-site.xml
+      fi;
     fi;
 done < .env
 rm hive/conf/.tmp
